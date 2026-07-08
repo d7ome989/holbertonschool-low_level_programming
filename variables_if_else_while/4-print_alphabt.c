98523c94341cd1d47c062a534a6eade822b7f150
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-08 11:13:39 +0300

    print a-z without e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and checks its value.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
