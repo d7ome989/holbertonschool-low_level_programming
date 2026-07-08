ba970786c1ccd64b8fe489dfb67a4edeeb84bcf9
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-08 11:18:52 +0300

    print num

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and checks its value.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d\n", n);
+	}
+	putchar('\n');
+	return (0);
+}
