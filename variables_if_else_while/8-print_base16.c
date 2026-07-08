f3d9c5eac77383825754109a064c2663b54f1008
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-08 11:46:46 +0300

    print HEXAdicmal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and checks its value.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+	char a;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
