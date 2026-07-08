1a37695075909958a263bfa3ce3d0d132ba53b8b
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-08 11:56:38 +0300

    print number 1 - 9 with SPASE and ',' mark

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and checks its value.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
