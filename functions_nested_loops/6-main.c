8068b7c0972e1da4d9d0f65cb0632eb4cc6c2833
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-09 20:22:45 +0300

    only intger

---
+++ functions_nested_loops/6-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", _abs(5));
+	printf("%d\n", _abs(0));
+	printf("%d\n", _abs(-77));
+	return (0);
+}
