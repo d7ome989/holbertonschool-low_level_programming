d30ce7f149a947828949460b358f250a71b9cef1
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-09 19:11:25 +0300

    Create funtion to check alpha

---
+++ functions_nested_loops/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", _isalpha('a'));
+	printf("%d\n", _isalpha('A'));
+	printf("%d\n", _isalpha('9'));
+	return (0);
+}
