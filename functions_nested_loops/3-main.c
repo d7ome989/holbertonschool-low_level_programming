ae0988178b69f4b0574463238ab7acd3e76598ff
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-09 14:39:29 +0300

    create funtion to check this carecter is small latter or not

---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", _islower('a'));
+	printf("%d\n", _islower('A'));
+	printf("%d\n", _islower('9'));
+	return (0);
+}
