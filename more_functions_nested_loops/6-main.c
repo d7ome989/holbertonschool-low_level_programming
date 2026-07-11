66b12ef718fbb8228efd26abf20ef289fc8219df
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-11 18:44:47 +0300

    this code for print _

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 0;
+	print_line(x);
+	return (0);
+}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - print _ this for mark _
+ * @n: this is number
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+
+		}
+		_putchar('\n');
+	}
+}
