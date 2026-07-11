42247c586c382f9a1c2856f3e683503eb13a40db
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-11 17:03:37 +0300

    this funtion print print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the product of a and b
+ */
+void print_numbers(void)
+{
+	int i;
+	for(i = 0; i <= 9; i++ )
+	{
+		_putchar( i + '0');
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _putchar(char c);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
-
+void print_numbers(void);
 #endif
