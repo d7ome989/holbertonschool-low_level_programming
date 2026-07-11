68bf753fb6a8f961139aa7b6e0fde39f355afc31
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-11 16:17:28 +0300

    this is code to use multibale

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0-9)
+ * @c: the character to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
