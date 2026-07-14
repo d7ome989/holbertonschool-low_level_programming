fd7eb62a86f16203db3dfd4c014fc3acf9563fe5
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 14:01:15 +0300

    print string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: the string  want to print
+ *
+ *
+ * Return: void
+ *
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
