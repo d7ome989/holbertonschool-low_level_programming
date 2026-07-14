c03f4cd2cbaf655e4911ff63f3634df1e1148891
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 14:17:30 +0300

    this is to print a rev of string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - print a string but rev
+ * @s: the string  want to print revrens
+ *
+ *
+ * Return: void
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
