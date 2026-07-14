ab8d57f625a895c965bc72ab67a59561c235c81e
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 17:50:05 +0300

    this code to print string but jumb 2 character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print a string
+ * @str: the string  want to print
+ *
+ *
+ * Return: void
+ *
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
