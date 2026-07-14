8b515b5e6bbf248ad0c17dd8f3211d5336af7458
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 18:32:47 +0300

    this code is print put start from half

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "01234567891011";
-    puts2(str);
+    str = "0123456789";
+    puts_half(str);
     return (0);
 }
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len;
+	int start;
+	int i;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	start = len / 2;
+
+	for (i = start; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
