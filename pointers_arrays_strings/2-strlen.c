c47ef2dc654288d9da71562708e135abf161bd11
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 13:50:01 +0300

    create a funtion count length in string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - count of a how many length in words
+ * @s: the string to be measured
+ *
+ *
+ * Return: the length of the string;
+ *
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+	}
+	return (i);
+}
