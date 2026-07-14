3f8533705cfbcab5fc4c9493132f794b9da96269
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 12:47:03 +0300

    change any number to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value pointed to by n to 98
+ * @n: this is a pointer
+ * Return: void
+ *
+ *
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+
+}
