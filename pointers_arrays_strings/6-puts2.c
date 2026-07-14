4a4fde19617a6bb4c0fecb935d4af5e4887fe2d0
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-14 17:58:17 +0300

    edit the code

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,8 +12,13 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	for (i = 0; len > i; i += 2)
 	{
 
 		_putchar(str[i]);
