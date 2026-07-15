b999bdfd0f6779b4dc56633f0a7433e8d1b8027a
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-15 22:49:23 +0300

    my calculater

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -3,6 +3,9 @@
 int main()
 {
 	int n;
+	int a;
+	int b;
+	int result;
 
 	printf("Welcome to my calculater\n");
 	printf("%s\n","1) Add");
@@ -15,6 +18,45 @@ int main()
 	{
 		printf("bye!\n");
 	}
+	else if (n == 1)
+	{
+		printf("ok pless enter Frist number .\n");
+		scanf("%d", &a);
+		printf("ok pless enter second number .\n");
+                scanf("%d", &b);
+		result = a + b;
+                printf("result is %d\n ", result);
+
+	}
+	else if (n == 2)
+        {
+                printf("ok pless enter Frist number .\n");
+                scanf("%d", &a);
+                printf("ok pless enter second number .\n");
+                scanf("%d", &b);
+                result = a - b;
+                printf("result is %d\n ", result);
+
+        }
+	else if (n == 3)
+        {
+                printf("ok pless enter Frist number .\n");
+                scanf("%d", &a);
+                printf("ok pless enter second number .\n");
+                scanf("%d", &b);
+                result = a * b;
+                printf("result is %d\n ", result);
+
+        }
+	else if (n == 4)
+        {
+                printf("ok pless enter Frist number .\n");
+                scanf("%d", &a);
+                printf("ok pless enter second number .\n");
+                scanf("%d", &b);
+                result = a / b;
+                printf("result is %d\n ", result);
+        }
 	else
 	{
 		printf("Invalid choice");
