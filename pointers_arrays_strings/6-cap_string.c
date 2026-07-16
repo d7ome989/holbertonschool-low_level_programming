d497e3024a90d84a9657c4b23a1b93aea8f33010
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-16 13:46:04 +0300

    check the if small later change to capital

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to modify
+ *
+ * Return: a pointer to str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
+		    str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
+		    str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+		    str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
