ee9c778a599a9a074ba78c5b898bdeb56137a184
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-16 12:01:14 +0300

    edit code to betty style

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,7 @@
-#include "main.h" 
+#include "main.h"
+
 /**
+ * _strcat - concatenates two strings
  * @dest: the destination buffer
  * @src: the source string
  *
@@ -7,18 +9,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
 
-        int i;
-        int j;
-
-        for (i = 0; dest[i] != '\0'; i++)
-        {
-        }
-        for (j = 0; src[j] != '\0'; j++, i++)
-        {
-                dest[i] = src[j];
-        }
-        dest[i] = '\0';
-
-        return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
