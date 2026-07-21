fef023f9e3d3568b3ce804f62378b2b1df6ac376
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-21 12:53:38 +0300

    edit code

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: the integer to print
+ * print_num - recursively prints the digits of a number
+ * @n: the number to print
  *
  * Return: nothing
  */
-void print_number(int n)
+void print_num(int n)
 {
 	int last_digit;
-
-	if (n < 0)
-	{
-		_putchar('-');
-	}
 
 	last_digit = n % 10;
 	if (last_digit < 0)
@@ -23,7 +18,22 @@ void print_number(int n)
 
 	if (n <= -10 || n >= 10)
 	{
-		print_number(n / 10);
+		print_num(n / 10);
 	}
 	_putchar(last_digit + '0');
+}
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	print_num(n);
 }
