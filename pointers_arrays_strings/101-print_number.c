5b054bd9196fab4525bb53f99f0eb70e65846e6e
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-21 12:50:43 +0300

    edit code

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,22 @@
  */
 void print_number(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
 	}
-	if (n >= 10)
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+
+	if (n <= -10 || n >= 10)
 	{
 		print_number(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(last_digit + '0');
 }
