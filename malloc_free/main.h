019716320accf1eef2e3e9a35254b397cb03f8c7
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-22 16:19:22 +0300

    add to main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -6,6 +6,6 @@ char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _putchar(char c);
-
+int **alloc_grid(int width, int height);
 
 #endif
