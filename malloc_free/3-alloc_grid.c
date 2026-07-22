336b4edd6be4db7893f0b08670b564b33ced0051
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-22 16:17:08 +0300

    this is for 3d

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * alloc_grid - allocates a 2D array of integers, initialized to 0
+ * @width: the number of columns in the grid
+ * @height: the number of rows in the grid
+ *
+ * Return: a pointer to the newly allocated 2D array, or NULL if
+ *         width or height is 0 or negative, or if malloc fails
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
---
+++ variables_if_else_while/sumtax.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("%d\n", 12%2 );
+	printf("%d\n", 12%3 );
+	printf("%d\n", 12%10);
+	printf("%d\n", 89%7);
+
+
+	return(0);
+}
