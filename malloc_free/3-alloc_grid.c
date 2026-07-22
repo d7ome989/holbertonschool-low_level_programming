ca2f81f90f0dd4f6490498049906479a9c5def10
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-22 16:25:51 +0300

    edit code for betty style

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * alloc_grid - allocates a 2D array of integers, initialized to 0
- * @width: the number of columns in the grid
- * @height: the number of rows in the grid
- *
- * Return: a pointer to the newly allocated 2D array, or NULL if
- *         width or height is 0 or negative, or if malloc fails
- */
+* alloc_grid - allocates a 2D array of integers, initialized to 0
+* @width: the number of columns in the grid
+* @height: the number of rows in the grid
+*
+* Return: a pointer to the newly allocated 2D array, or NULL if
+*         width or height is 0 or negative, or if malloc fails
+*/
 int **alloc_grid(int width, int height)
 {
 	int **grid;
+
 	int i;
+
 	int j;
 
 	if (width <= 0 || height <= 0)
