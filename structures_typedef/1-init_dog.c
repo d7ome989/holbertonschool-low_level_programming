6eaa652dea93e7a534475ad0b66c79a70858f2f0
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-25 21:47:54 +0300

    the send project

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a dog structure.
+ * @d: Pointer to the dog structure to initialize.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d == NULL)
+        return;
+
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include "stdio.h"
 /**
-* struct dog - Structure defining a dog.
-* @name: The name of the dog.
-* @age: The age of the dog.
-* @owner: The owner of the dog.
-*/
+ * struct dog - Structure defining a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
-	char *name;
-
-	float age;
-
-	char *owner;
-
+    char *name;
+    float age;
+    char *owner;
 };
+
+/**
+ * init_dog - Initializes a dog structure.
+ * @d: Pointer to the dog structure to initialize.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
