ab9f96ad592eef4374147a6e1dc7106ebb605dca
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-25 22:55:08 +0300

    this to print dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,29 +1,32 @@
 #ifndef DOG_H
 #define DOG_H
-#include "stdio.h"
+
 /**
-* struct dog - Structure defining a dog.
-* @name: The name of the dog.
-* @age: The age of the dog.
-* @owner: The owner of the dog.
-*/
+ * struct dog - Structure defining a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
-
 	float age;
-
 	char *owner;
-
 };
 
 /**
-* init_dog - Initializes a dog structure.
-* @d: Pointer to the dog structure to initialize.
-* @name: The name of the dog.
-* @age: The age of the dog.
-* @owner: The owner of the dog.
-*/
+ * init_dog - Initializes a dog structure.
+ * @d: Pointer to the dog structure to initialize.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints information about a dog.
+ * @d: Pointer to the dog structure to print.
+ */
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
