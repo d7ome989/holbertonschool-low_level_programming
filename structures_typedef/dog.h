2c68db903866550a1e909897747bd4a757172052
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-25 21:44:11 +0300

    edit to betty style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,20 @@
-/* Define a new type struct dog */
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Structure defining a dog.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+*/
 struct dog
 {
 	char *name;
+
 	float age;
+
 	char *owner;
+
 };
+
+#endif /* DOG_H */
