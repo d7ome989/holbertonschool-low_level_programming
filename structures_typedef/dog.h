e81b47c2ccce2da8d664de154cbe5bc6609840da
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-25 22:47:54 +0300

    edit to betty style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,25 +2,28 @@
 #define DOG_H
 #include "stdio.h"
 /**
- * struct dog - Structure defining a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
+* struct dog - Structure defining a dog.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+
+	float age;
+
+	char *owner;
+
 };
 
 /**
- * init_dog - Initializes a dog structure.
- * @d: Pointer to the dog structure to initialize.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
+* init_dog - Initializes a dog structure.
+* @d: Pointer to the dog structure to initialize.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
