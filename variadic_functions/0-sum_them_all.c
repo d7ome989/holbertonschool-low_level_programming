518e18faa777f3c166add86d33e09cb911ca4304
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-28 19:08:22 +0300

    this is a frist  in validation

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: the number of parameters
+ *
+ * Return: the sum of all parameters, or 0 if n is 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+	int count = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+		count += va_arg(num, int);
+	va_end(num);
+
+	return (count);
+}
