3564b7a58b28d4bbf084249b58a6b6c1111d8c9e
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-29 13:42:25 +0300

    this is to print in new line

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: array of arguments passed to the program
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
