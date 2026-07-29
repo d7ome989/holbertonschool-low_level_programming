f6d26cea946350c305931ec39b8ff367f9a0ac1e
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-29 11:50:23 +0300

    this is to print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints a char from a va_list
+ * @ap: the va_list containing the value
+ *
+ * Return: void
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int - prints an int from a va_list
+ * @ap: the va_list containing the value
+ *
+ * Return: void
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints a float from a va_list
+ * @ap: the va_list containing the value
+ *
+ * Return: void
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - prints a string from a va_list, or (nil) if NULL
+ * @ap: the va_list containing the value
+ *
+ * Return: void
+ */
+void print_string(va_list ap)
+{
+	char *str;
+
+	str = va_arg(ap, char *);
+	if (str == NULL)
+		printf("(nil)");
+	printf("%s", str == NULL ? "" : str);
+}
+
+/**
+ * print_all - prints anything, based on a format string
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	fmt_t table[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+	va_list ap;
+	unsigned int i, j;
+
+	i = 0;
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (table[j].type != '\0')
+		{
+			if (table[j].type == format[i])
+				table[j].f(ap);
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,6 +9,12 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+typedef struct fmt
+{
+	char type;
+	void (*f)(va_list ap);
+} fmt_t;
 
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
