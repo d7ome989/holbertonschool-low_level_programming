caed7c59263d4393ce6899deb496adb94ffeff86
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-07-29 13:27:14 +0300

    edit code for return and add void(argc)

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the first argument passed to the program
- * @argc: the number of arguments
- * @argv: array of arguments
+ * main - prints the name of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: array of arguments passed to the program
  *
- * Return: always 0
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
 
-	return (argc);
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
