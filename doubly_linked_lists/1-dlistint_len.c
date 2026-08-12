ca9c2a79a58078fc028bb477996979bcf997c358
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-12 11:31:25 +0300

    edit code remove the int n

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -9,7 +9,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *current = h;
-int n = 0;
+
 	for (count = 0; current != NULL; count++)
 	{
 		current = current->next;
