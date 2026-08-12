879cf6abf0651e4e41ec12af99f48c3414980426
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-12 11:29:57 +0300

    print len of duble linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns number of a dlistint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+int n = 0;
+	for (count = 0; current != NULL; count++)
+	{
+		current = current->next;
+	}
+
+	return (count);
+}
