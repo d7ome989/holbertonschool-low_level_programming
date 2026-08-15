0990acb57ea291fa1432ee8b74c39e96ce897c04
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-15 16:49:38 +0300

    edit betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,27 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: the hash table
- * @key: the key (cannot be an empty string)
- * @value: the value associated with the key (must be duplicated)
+ * update_value - updates the value of an existing key in a hash node list
+ * @current: pointer to the head of the list at the hash index
+ * @key: the key to search for
+ * @value: the new value to set
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: 1 if the key was found and updated, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int update_value(hash_node_t *current, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node;
-	hash_node_t *current;
-
-	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
-	{
-		return (0);
-	}
-
-	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
-
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
@@ -35,6 +23,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 		current = current->next;
+	}
+
+	return (0);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table
+ * @key: the key (cannot be an empty string)
+ * @value: the value associated with the key (must be duplicated)
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
+	{
+		return (0);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (update_value(ht->array[index], key, value))
+	{
+		return (1);
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
