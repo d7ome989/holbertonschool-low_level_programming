e29ea297aa0b8b150b9551c2799aaff78e444cd8
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-16 14:53:45 +0300

    chaeck plidation

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_palindrome_helper - checks if s is a palindrome between i and j
+ * @s: the string
+ * @i: index from the start
+ * @j: index from the end
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int i, int j)
+{
+	if (i >= j)
+		return (1);
+
+	if (s[i] != s[j])
+		return (0);
+
+	return (is_palindrome_helper(s, i + 1, j - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+
+	return (is_palindrome_helper(s, 0, len - 1));
+}
