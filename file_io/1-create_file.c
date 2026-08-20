160a83ee83100dc0da20d35a0676f7d5c45c352c
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-20 17:25:41 +0300

    create file in i\o

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with given content
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+	if (i > 0)
+	{
+		w = write(fd, text_content, i);
+		if (w == -1 || w != i)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
