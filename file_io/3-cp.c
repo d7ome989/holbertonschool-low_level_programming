3866afdb22b256438b24b9d52cd7e6b7b6039dc1
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-20 17:39:54 +0300

    edit betty style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,6 +6,54 @@
 #include <unistd.h>
 
 /**
+ * copy_file - copies the content of one file to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @name: destination file name
+ *
+ * Return: 0 on success, 98 on read error, 99 on write error
+ */
+int copy_file(int fd_from, int fd_to, char *name)
+{
+	char buffer[1024];
+	int r;
+	int w;
+	int total;
+
+	r = read(fd_from, buffer, 1024);
+
+	while (r > 0)
+	{
+		total = 0;
+
+		while (total < r)
+		{
+			w = write(fd_to, buffer + total, r - total);
+
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", name);
+				return (99);
+			}
+
+			total += w;
+		}
+
+		r = read(fd_from, buffer, 1024);
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", name);
+		return (98);
+	}
+
+	return (0);
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: arguments
@@ -14,11 +62,8 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to;
-	int r, w;
-	int close_from, close_to;
-	int total;
-	char buffer[1024];
+	int fd_from;
+	int fd_to;
 
 	if (argc != 3)
 	{
@@ -39,59 +84,32 @@ int main(int argc, char **argv)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", argv[2]);
+
 		if (close(fd_from) == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't close fd %d\n", fd_from);
 			return (100);
 		}
+
 		return (99);
 	}
 
-	r = read(fd_from, buffer, 1024);
-
-	while (r > 0)
+	if (copy_file(fd_from, fd_to, argv[2]) != 0)
 	{
-		total = 0;
-
-		while (total < r)
-		{
-			w = write(fd_to, buffer + total, r - total);
-
-			if (w == -1)
-			{
-				dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
-				close(fd_from);
-				close(fd_to);
-				return (99);
-			}
-
-			total += w;
-		}
-
-		r = read(fd_from, buffer, 1024);
-	}
-
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		return (98);
+		return (99);
 	}
 
-	close_from = close(fd_from);
-	if (close_from == -1)
+	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't close fd %d\n", fd_from);
 		return (100);
 	}
 
-	close_to = close(fd_to);
-	if (close_to == -1)
+	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't close fd %d\n", fd_to);
